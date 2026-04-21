734e966b78eab8f223bcb147233071a3531ebb93
vidyaraveendran <vidyaraveendran2000@gmail.com>
2026-04-21 14:34:57 +0530

    fix port

--- backend/server.js
+++ backend/server.js
@@ -76,6 +76,8 @@ app.delete("/users/:id", async (req, res) => {
 
 
 // 👇 keep this at bottom(START SERVER)
-app.listen(5000, () => {
-  console.log("Server running on port 5000");
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
